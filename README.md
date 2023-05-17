50e34206f7e0d803bdd1d090ece7c06b9074165a
Phol <Pholibertlim@gmail.com>
2023-05-18 02:42:18 +0800

    feat: results summary component

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Result_Summary_Component
+# Result Summary Component
+
+## Overview
+
+Frontend web development
+
+### Screenshot
+
+Mobile View
+
+Desktop View
+
+
+### Links
+
+- Source Code: 
+
+- Live Site: 
+
+### My process
+
+- Semantic HTML5 markup
+
+- CSS Custom properties
+
+- Flexbox
+
+- Mobile-first workflow
+
+- JS
+
+### What I learned
+
+I learned that basic fundamentals are very important and to honed more advance tools, you need to master or understand the basics
+
+### Author
+
+- Website - [Phol](https://phol.vercel.app/)
---
+++ displayDetails.js
@@ -0,0 +1,37 @@
+'use strict';
+
+//using import assertion to be able to read the imported json file.
+import data from './data.json' assert { type: 'json' };
+
+const display = document.querySelector('.dataResult');
+const colorFunction = (category) => {
+  return category === 'Reaction'
+    ? 'lightRed'
+    : category === 'Memory'
+    ? 'orangeYellow'
+    : category === 'Verbal'
+    ? 'greenTeal'
+    : category === 'Visual'
+    ? 'cobaltBlue'
+    : null;
+};
+
+const displayResult = data
+  .map((result) => {
+    return `
+  <div class='resultContainer ${colorFunction(result.category)}'>
+  <section class="resultText">
+  <img src='${result.icon}' alt='Icon'/>
+  <h3>${result.category}</h3>
+  </section>
+  <p class='darkGrayBlue'>${result.score} 
+  <span class='lightColorText'>/ 100</span>
+  </p>
+  </div>
+  `;
+  })
+  .join('');
+
+display.innerHTML = displayResult;
+
+console.log(data);
